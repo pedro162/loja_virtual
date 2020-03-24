fdaa9fd018cf40ad8249b5534ca389773f5dcbe6
José Pedro Aguiar Ferreira <phedroclooney@gmail.com>
2020-03-24 18:12:49 -0300

    subindo sistema de grid dimanico

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,7 +12,43 @@ class HomeController extends BaseController
     {
         $produto = new Produto();
         
-        $this->view->produtos = $produto->select(['nomeProduto','textoPromorcional']);
+
+        $resultSelect = $produto->select(['nomeProduto','textoPromorcional', 'idProduto']);
+        $gridProdutos = [];
+
+        if((count($resultSelect) % 2) ==0)
+        {
+           for ($i=0; !($i == count($resultSelect)); $i+=4) {
+
+                $subArray = [];
+
+                $sentinela = 0;
+                while (!($sentinela == 4)) {
+                    $subArray[] = $resultSelect[$i + $sentinela];
+                    $sentinela ++;
+                }
+                $gridProdutos[] = $subArray;
+            } 
+        }
+        else{
+           for ($i=0; !($i == count($resultSelect)); $i+=3) {
+
+                $subArray = [];
+
+                $sentinela = 0;
+                while (!($sentinela == 3)) {
+                    $subArray[] = $resultSelect[$i + $sentinela];
+                    $sentinela ++;
+                }
+                $gridProdutos[] = $subArray;
+            } 
+        }
+        
+
+       // echo "<pre>";
+       // var_dump($gridProdutos);
+       // echo "</pre>";
+        $this->view->produtos = $gridProdutos;
         $this->render('home/home', true);
     }
 
