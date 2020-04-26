c0f08030571d14e00100310c45586c92c7e360ad
phedro aguiar <phedroclooney@gmail.com>
2020-04-25 21:15:38 -0300

    Ajustando click do bnt desempenho

--- public/js/valida_login.js
+++ public/js/valida_login.js
@@ -315,7 +315,8 @@ $(document).ready(function(){
 
 
   /*------------------------ Disparando funcoes dos gráficos -----------------------*/
-  $('#btnInicio').on('click', function(){
+  $('#desemprenho').on('click', function(){
+    $('#dinamic').html('');
     $('#dinamic').css('background-color', 'rgba(0, 0, 0, .8)')
     $('#dinamic').append($('<div/>').addClass('col-md-6').css('margin-rith', '2px').append($('<div/>').text('Meta').addClass('titleChar h3')).append($('<canvas/>').attr('id', 'metaVenda')));
     $('#dinamic').append($('<div/>').addClass('col-md-6').append($('<div/>').text('Compartivo do exercicio anual').addClass('titleChar h3')).append($('<canvas/>').attr('id', 'excAnual')));
